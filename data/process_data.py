afb47ba7b7e4fdadf916190219537d725fff261f
SimplifyData <atif.zafar@sds.technology>
2019-02-10 14:07:38 -0700

    Scripting ETL Piline py file

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,51 @@
 import sys
+import pandas as pd
+import numpy as np
 
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages =  pd.read_csv(message_filepath)
+    categories = pd.read_csv(categories_filepath, sep = ",")
+    df = pd.concat([messages, categories], axis = 1)
+    return df
 
 
 def clean_data(df):
-    pass
+    categories = df.categories.str.split(";", expand = True)
+    
+    #column names for categories
+    category_colnames = list(map(lambda x : x[:-2] ,row))
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        #print(column)
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        
+        # drop the original categories column from `df`
+    df.drop(["categories"],axis = 1,  inplace = True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df_1 = pd.concat([df,categories], axis = 1)
+    
+    # drop duplicates
+    df_1.drop("id", axis =1,inplace = True)
+    
+    # check number of duplicates
+    df_1["id"] = messages.id
+    
+    return df_1
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    
+    df.to_sql('Disaster_Response', engine, index=False, chunksize = 500) 
 
 
 def main():
