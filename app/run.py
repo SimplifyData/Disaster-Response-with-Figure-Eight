4abccf7206c69afc0707ba42b19b2928bac5094b
SimplifyData <atif.zafar@sds.technology>
2019-02-10 21:10:09 -0700

    updated model pickel path

--- app/run.py
+++ app/run.py
@@ -30,7 +30,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('Disaster_Response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/model_pipeline.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
